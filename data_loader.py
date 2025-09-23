9ef80f2a960b00a1d595f107da337734485f0558
ponomarets-chem <ponomarets@scamt-itmo.ru>
2025-09-23 13:31:34 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import wget
+import gdown
 
 #строчка выше нужна потому что файл не открывается в браузере он слишком большой
 # сюда вставила последний кусок ссылки - это айди. Без хвостика в конце
@@ -8,11 +8,11 @@ FILE_ID = "1CcFVavXoBoMwNZRdlXCKA-LY8l8kU_kR"
 # ссылку для скачивания
 url = f"https://drive.google.com/uc?id={FILE_ID}"
 
-# сначала скачиваем файл в папку
-filename = wget.download(url, "data.csv")
+# сначала скачиваем файл локально, он слишком большой для прямого чтения
+gdown.download(url, "data.csv", quiet=False)
 
 # читаем CSV по ссылке
-raw_data = pd.read_csv(url)
+raw_data = pd.read_csv("data.csv")
 
 # показываем первые 10 строк
 print(raw_data.head(10))
