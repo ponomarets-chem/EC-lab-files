2acb576d6e40cb424b5223407d523fe6c134ac9a
ponomarets-chem <ponomarets@scamt-itmo.ru>
2025-09-23 13:51:49 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -2,23 +2,20 @@ import os
 import pandas as pd
 import gdown
 
-#строчка выше нужна потому что файл не открывается в браузере он слишком большой
-# сюда вставила последний кусок ссылки - это айди. Без хвостика в конце
-FILE_ID = "1YF8duBM5HERkyCPAUPlzrs9mirZInNkT"  
-
-# ссылку для скачивания
+FILE_ID = "1YF8duBM5HERkyCPAUPlzrs9mirZInNkT"
 url = f"https://drive.google.com/uc?id={FILE_ID}"
 local_filename = "инжиниринг.csv"
+
 # Скачиваем файл, если его нет
-# -----------------------------
 if not os.path.exists(local_filename):
     print("Файл не найден локально. Скачиваем...")
     gdown.download(url, local_filename, quiet=False)
 else:
     print("Файл уже существует. Используем локальный файл.")
-    
-    # Пробуем открыть с стандартными настройками
-    raw_data = pd.read_csv(local_filename)
 
-# показываем первые 10 строк
+# Чтение CSV с локального файла
+raw_data = pd.read_csv(local_filename)
+
+# Показ первых 10 строк
 print(raw_data.head(10))
+
