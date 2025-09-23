40ca2f1ae37577455d895d500bd46247591ee3fd
ponomarets-chem <ponomarets@scamt-itmo.ru>
2025-09-23 13:40:48 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import gdown
 
@@ -7,12 +8,17 @@ FILE_ID = "1CcFVavXoBoMwNZRdlXCKA-LY8l8kU_kR"
 
 # ссылку для скачивания
 url = f"https://drive.google.com/uc?id={FILE_ID}"
-
-# сначала скачиваем файл локально, он слишком большой для прямого чтения
-gdown.download(url, "data.csv", quiet=False)
-
-# читаем CSV по ссылке
-raw_data = pd.read_csv("data.csv")
+local_filename = "data.csv"
+# Скачиваем файл, если его нет
+# -----------------------------
+if not os.path.exists(local_filename):
+    print("Файл не найден локально. Скачиваем...")
+    gdown.download(url, local_filename, quiet=False)
+else:
+    print("Файл уже существует. Используем локальный файл.")
+try:
+    # Пробуем открыть с стандартными настройками
+    raw_data = pd.read_csv(local_filename)
 
 # показываем первые 10 строк
 print(raw_data.head(10))
