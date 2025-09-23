b74071a06c44b8d32d075c4dded730314ac63652
ponomarets-chem <ponomarets@scamt-itmo.ru>
2025-09-23 13:03:01 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
-FILE_ID = "https://drive.google.com/drive/u/1/folders/1CcFVavXoBoMwNZRdlXCKA-LY8l8kU_kR?ths=true" 
+# сюда вставила последний кусок ссылки - это айди
+FILE_ID = "1CcFVavXoBoMwNZRdlXCKA-LY8l8kU_kR?ths=true"  
 
+# ссылку для скачивания
+url = f"https://drive.google.com/drive/u/1/folders/={FILE_ID}"
+
+# читаем CSV по ссылке
 raw_data = pd.read_csv(url)
 
+# показываем первые 10 строк
 print(raw_data.head(10))
