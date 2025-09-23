d3513eb311f8c94ecabf28211d77a5b9ccb7eb05
ponomarets-chem <ponomarets@scamt-itmo.ru>
2025-09-23 13:25:58 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,10 +1,15 @@
 import pandas as pd
+import wget
 
+#строчка выше нужна потому что файл не открывается в браузере он слишком большой
 # сюда вставила последний кусок ссылки - это айди. Без хвостика в конце
 FILE_ID = "1CcFVavXoBoMwNZRdlXCKA-LY8l8kU_kR"  
 
 # ссылку для скачивания
 url = f"https://drive.google.com/uc?id={FILE_ID}"
+
+# сначала скачиваем файл в папку
+filename = wget.download(url, "data.csv")
 
 # читаем CSV по ссылке
 raw_data = pd.read_csv(url)
