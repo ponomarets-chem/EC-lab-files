f0d712b01a9cee1dfda214d42993f004a3de994e
ponomarets-chem <ponomarets@scamt-itmo.ru>
2025-09-30 14:07:47 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,17 +1,11 @@
 import os
 import pandas as pd
 import gdown
-import sys
 
 FILE_ID = "1YF8duBM5HERkyCPAUPlzrs9mirZInNkT"
 url = f"https://drive.google.com/uc?id={FILE_ID}"
 local_filename = "инжиниринг.csv"
 out_parquet = "инжиниринг.parquet"
-# Список типов: mode	0	error	control changes	Ns changes	counter inc.	Ns	time/s	
-# control/V	Ewe/V	<I>/mA	dQ/C	(Q-Qo)/C	I Range	Q charge/discharge/mA.h	
-# half cycle	Energy charge/W.h	Energy discharge/W.h	Capacitance charge/µF	
-# Capacitance discharge/µF	Q discharge/mA.h	Q charge/mA.h	Capacity/mA.h	
-# Efficiency/%	cycle number	P/W
 
 TYPE_MAP = {
     "Source.Name": "category",
@@ -43,14 +37,13 @@ TYPE_MAP = {
     "P/W": "float"
 }
 
-# Проверка и скачивания файлв
-if not os.path.exists(local_filename):
-    print("Файл не найден локально. Скачиваем...")
-    gdown.download(url, local_filename, quiet=False)
-else:
-    print("Файл уже существует. Используем локальный файл.")
+def download_if_needed():
+    if not os.path.exists(local_filename):
+        print("Файл не найден локально. Скачиваем...")
+        gdown.download(url, local_filename, quiet=False)
+    else:
+        print("Файл уже существует. Используем локальный файл.")
 
-# Чтение и приведение типов
 def load_and_cast():
     print("Читаем CSV...")
     df = pd.read_csv(local_filename, low_memory=False)
@@ -66,22 +59,17 @@ def load_and_cast():
             print(f"⚠️ ВНИМАНИЕ: колонки {col} нет в файле!")
     return df
 
-# Сохранение в parquet
-
 def save_parquet(df):
     print("Сохраняем в Parquet:", out_parquet)
     df.to_parquet(out_parquet, engine="pyarrow", compression="snappy", index=False)
     print("Файл сохранён:", out_parquet)
 
 def main():
-def download_if_needed():
-    if not os.path.exists(local_filename):
-        print("Файл не найден локально. Скачиваем...")
-        gdown.download(url, local_filename, quiet=False)
-    else:
-        print("Файл уже существует. Используем локальный файл.")
+    download_if_needed()
+    df = load_and_cast()
+    print("Первые 10 строк:")
+    print(df.head(10))
+    save_parquet(df)
 
 if __name__ == "__main__":
     main()
-
-
